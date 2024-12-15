4c4674f9a3088efb244c48ab0c7eac400155d2af
Johan Hedin <johan.hedin@omda.com>
2024-12-15 23:31:31 +0100

    Finds counters

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/johanatandromeda/nftables-exporter/pkg/nftables"
 	"log/slog"
 	"os"
 )
+
+var version = ""
 
 func main() {
 
 	var programLevel = new(slog.LevelVar) // Info by default
 	h := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: programLevel})
 	slog.SetDefault(slog.New(h))
-	nftables.Test()
+
+	slog.Info(fmt.Sprintf("Starting nftables-exporter V %s\n", version))
+
+	debug := flag.Bool("d", false, "Debug")
+
+	flag.Parse()
+
+	if *debug {
+		programLevel.Set(slog.LevelDebug)
+	}
+
+	_, err := nftables.GetCounter()
+	if err != nil {
+		panic(err)
+	}
 }
--- pkg/nftables/nftables.go
+++ pkg/nftables/nftables.go
@@ -1,10 +1,39 @@
 package nftables
 
-import "github.com/google/nftables"
+import (
+	"fmt"
+	"github.com/google/nftables"
+	"github.com/google/nftables/expr"
+	"log/slog"
+)
 
-func Test() {
-	_, err := nftables.New()
+func GetCounter() (map[string]uint64, error) {
+
+	c, err := nftables.New()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+
+	chains, err := c.ListChains()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, chain := range chains {
+		chainName := chain.Name
+		slog.Debug(fmt.Sprintf("Processing chain %s", chainName))
+		rules, err := c.GetRules(chain.Table, chain)
+		if err != nil {
+			return nil, err
+		}
+		for _, rule := range rules {
+			for _, ex := range rule.Exprs {
+				count, ok := ex.(*expr.Counter)
+				if ok {
+					slog.Debug(fmt.Sprintf("Got count with packages %v and bytes %v", count.Packets, count.Bytes))
+				}
+			}
+		}
+	}
+	return nil, nil
 }
